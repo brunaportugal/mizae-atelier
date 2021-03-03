ad5429fbc507957386d408049d365c1422f14798
Bruna Portugal <brunabportugal@gmail.com>
2021-03-03 14:07:22 +0100

    First seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Collection.destroy_all
+Product.destroy_all
+
+puts "Creating collections"
+
+collection_one = Collection.create!(name: "Casa")
+collection_two = Collection.create!(name: "Jóias")
+
+puts "Collections created"
+puts "Creating products"
+
+product = Product.create!(name: "Vaso de cerâmica", description: "Vaso feito à mão", price: "R$ 300,00", availability: "Em estoque", category: "Vasos e Esculturas", collection: collection_one)
+
+puts "Products created"
